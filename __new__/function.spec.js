be8bc356ed5daca9c90abb867b12ac20edd6c023
shibo <576504437@q.com>
2019-04-03 16:25:29 +0800

    :rocket: test expect tobe

--- __new__/function.spec.js
+++ __new__/function.spec.js
@@ -6,14 +6,30 @@ test('sum (1,2) tobe 3',()=>{
   expect(sum(1,2)).toBe(3)
 })
 
-import React from 'react'
+import React,{Component} from 'react'
 import {shallow} from 'enzyme'
 import CheckTag from '../src/components/CheckTag'
 
-const checkTagComp = shallow(<CheckTag></CheckTag>)
+const checkTagComp = shallow(<CheckTag checked={true}>123</CheckTag>)
 
 describe('checktag sth',()=>{
-  it("chekctag is render with div.ant-tag-checkable ",()=>{
-    expect(checkTagComp.find('div.ant-tag-checkable').exists())
-  })
+	it("chekctag is render with div.ant-tag-checkable ",()=>{
+		expect(checkTagComp.find("div.ant-tag-checkable").exists())
+	})
+
+	it("checktag props test",()=>{
+		expect(checkTagComp.prop("checked")).toBeTruthy()
+	})
+
+	it("checktag children text match",()=>{
+		expect("123").toMatch(checkTagComp.prop("children"))
+	})
+
+	it("checktag instanceof what",()=>{
+		expect(checkTagComp.instance()).toBeInstanceOf(CheckTag)
+	})
+
+	it("checktag prop change",()=>{
+		const checkTagComp = shallow(<CheckTag>aaa</CheckTag>)
+	})
 })
