3902e28024faf6f784245c693fe00c61f256ed9f
shibo <576504437@q.com>
2019-04-08 10:17:40 +0800

    :rocket: test reducer & actions

--- __new__/function.spec.js
+++ __new__/function.spec.js
@@ -32,4 +32,34 @@ describe('checktag sth',()=>{
 	it("checktag prop change",()=>{
 		const checkTagComp = shallow(<CheckTag>aaa</CheckTag>)
 	})
+
+  /*测试是否被调用props传入的方法 toBeCalled*/
+  // it('When the Enter key was pressed, onAddClick() shoule be called', () => {
+  //   // mock input 输入和 Enter事件
+  //   const mockEvent = {
+  //     keyCode: 13, // enter 事件
+  //     target: {
+  //       value: 'Test'
+  //     }
+  //   }
+  //   // 通过 Enzyme 提供的 simulate api 模拟 DOM 事件
+  //   wrapper.find('input').simulate('keyup',mockEvent)
+  //   // 判断 props.onAddClick 是否被调用
+  //   expect(props.onAddClick).toBeCalled()
+  // })
 })
+
+/*reducer测试*/
+// import * as actions from '../../src/actions/index'
+// import * as types from '../../src/constants/actionTypes'
+//
+// describe('actions', () => {
+//   it('should create an action to add a todo', () => {
+//     const text = 'Finish docs'
+//     const expectedAction = {
+//       type: types.ADD_TODO,
+//       text
+//     }
+//     expect(actions.addTodo(text)).toEqual(expectedAction)
+//   })
+// })
